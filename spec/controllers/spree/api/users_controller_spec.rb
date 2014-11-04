4e56b3bf1d478edb554a21a2f7a9105aad92682b
Jaime Victoria <jaime.victoria@crowdint.com>
2014-11-04 12:29:01 -0600

    added create user with channel and image specs

--- spec/controllers/spree/api/users_controller_spec.rb
+++ spec/controllers/spree/api/users_controller_spec.rb
@@ -11,8 +11,10 @@ describe Spree::Api::UsersController do
       {
           user: {
               email:                 'new@example.com',
+              channel:               '1234asdf',
               password:              'spree123',
-              password_confirmation: 'spree123'
+              password_confirmation: 'spree123',
+              image_url:             'http://example.com/image.png'
           }
       }
     end
@@ -23,6 +25,16 @@ describe Spree::Api::UsersController do
     it 'can create a new user' do
       api_post :create, user_params
       expect(json_response['email']).to eq 'new@example.com'
+    end
+
+    it 'returns a channel' do
+      api_post :create, user_params
+      expect(json_response['channel']).to eq '1234asdf'
+    end
+
+    it 'returns an image url' do
+      api_post :create, user_params
+      expect(json_response['image_url']).to eq 'http://example.com/image.png'
     end
 
     context 'when admin param is set' do

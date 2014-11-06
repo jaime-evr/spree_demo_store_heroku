32b612549d2f577ca637d54b323a50953089e9d6
Jaime Victoria <jaime.victoria@crowdint.com>
2014-11-06 13:59:16 -0600

    Size of a normal product image is now 310x310

---
+++ app/decorators/models/spree/image_decorator.rb
@@ -0,0 +1,9 @@
+Spree::Image.class_eval do
+  attachment_definitions[:attachment][:styles] = {
+    :mini => '48x48>',
+    :small => '100x100>',
+    :product => '310x310>',
+    :large => '600x600>'
+  }
+end
+

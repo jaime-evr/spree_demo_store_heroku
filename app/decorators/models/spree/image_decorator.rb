87b179ea023fba8ebd4b1b275285748296660192
Jaime Victoria <jaime.victoria.90@gmail.com>
2014-11-06 20:12:01 -0600

    Use 320x320 for product size

--- app/decorators/models/spree/image_decorator.rb
+++ app/decorators/models/spree/image_decorator.rb
@@ -2,7 +2,7 @@ Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>',
     :small => '100x100>',
-    :product => '310x310>',
+    :product => '320x320>',
     :large => '600x600>'
   }
 end

3f174d0b60e17f262d6809907ec49cf3034e6ae9
Jaime Victoria <jaime.victoria@crowdint.com>
2014-11-07 18:41:08 -0600

    Retrieve all uniq products from all days of a menu

--- app/decorators/controllers/spree/api/products_controller_decorator.rb
+++ app/decorators/controllers/spree/api/products_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Api::ProductsController.class_eval do
   def index
-    @menu_products ||= @active_menu.menu_of_the_day
+    @menu_products ||= @active_menu.products
     respond_with(@menu_products)
   end
 

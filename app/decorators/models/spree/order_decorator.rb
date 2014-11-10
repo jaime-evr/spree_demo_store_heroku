f42e885321c64a3c2999314c9bd7698b0a0fbd9b
Jaime Victoria <jaime.victoria@crowdint.com>
2014-11-10 14:10:33 -0600

    Allow confirm order to be canceled

---
+++ app/decorators/models/spree/order_decorator.rb
@@ -0,0 +1,11 @@
+Spree::Order.class_eval do
+  def allow_cancel?
+    return false unless on_cancelable_step? and state != 'canceled'
+    shipment_state.nil? || %w{ready backorder pending}.include?(shipment_state)
+  end
+
+  private
+    def on_cancelable_step?
+      completed? || confirm?
+    end
+end

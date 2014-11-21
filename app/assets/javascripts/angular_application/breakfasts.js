2c487747f140c7bbceaabc104c18cc29e4dac1bb
Jaime Victoria <jaime.victoria@crowdint.com>
2014-11-21 12:03:11 -0600

    Hash syntax to prevent error on production minification

--- app/assets/javascripts/angular_application/breakfasts.js
+++ app/assets/javascripts/angular_application/breakfasts.js
@@ -1,16 +1,18 @@
 window.App = angular.module('Breakfasts', ['templates', 'ngResource', 'ngRoute']);
 
-App.config(function($httpProvider, $routeProvider) {
-  $httpProvider.interceptors.push('authInterceptor');
+App.config(['$httpProvider', '$routeProvider',
+  function($httpProvider, $routeProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
 
-  $routeProvider.when('/login', {
-    templateUrl: 'angular_application/templates/sign_in.html',
-    controller: 'AuthenticationsCtrl',
-  }).when('/home', {
-    templateUrl: 'angular_application/templates/home.html',
-    controller: 'HomeCtrl'
-  }).otherwise({
-    redirectTo: '/login'
-  });
-});
+    $routeProvider.when('/login', {
+      templateUrl: 'angular_application/templates/sign_in.html',
+      controller: 'AuthenticationsCtrl',
+    }).when('/home', {
+      templateUrl: 'angular_application/templates/home.html',
+      controller: 'HomeCtrl'
+    }).otherwise({
+      redirectTo: '/login'
+    });
+  }
+]);
 

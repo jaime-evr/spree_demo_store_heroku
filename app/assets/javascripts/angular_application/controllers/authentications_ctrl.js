cfd1ecd530869a1de8c6583f6e13dff0e957d20d
Jaime Victoria <jaime.victoria@crowdint.com>
2014-11-24 16:56:44 -0600

    use auth to sign in user after clicking the already rendered button

--- app/assets/javascripts/angular_application/controllers/authentications_ctrl.js
+++ app/assets/javascripts/angular_application/controllers/authentications_ctrl.js
@@ -2,56 +2,48 @@ App.controller('AuthenticationsCtrl', ['$scope', '$routeParams', '$location', '$
   function($scope, $routeParams, $location, $window, CreateUser, AuthenticateUser) {
     $scope.signedIn = false;
 
-    $scope.processAuth = function(authResult) {
-      if(authResult['access_token']) {
-        $scope.signedIn = true;
-        $scope.getUserInfo();
-      } else if(authResult['error']) {
-        $scope.signedIn = false;
-      }
-    };
-
-    $scope.signInCallback = function(authResult) {
+    $scope.processAuthResponse = function(authResult) {
       $scope.$apply(function() {
-        $scope.processAuth(authResult);
+        if(authResult['access_token']) {
+          $scope.signedIn = true;
+          $scope.getUserInfo();
+        } else if(authResult['error']) {
+          $scope.signedIn = false;
+        }
       });
     };
 
-    $scope.renderSignInButton = function() {
-      gapi.signin.render('signInButton',
-        {
-         'callback': $scope.signInCallback,
-         'clientid': '667263210901-g4gv6vkuhsbmm5srqe0jji8va549bbd9.apps.googleusercontent.com',
-         'requestvisibleactions': 'http://schemas.google.com/AddActivity',
-         'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email',
-         'cookiepolicy': 'single_host_origin'
-        }
-      );
-    }
-
-    $scope.login = function() {
-      $scope.renderSignInButton();
+    $scope.signIn = function() {
+      gapi.auth.signIn({
+        'callback': $scope.processAuthResponse,
+        'clientid': '667263210901-g4gv6vkuhsbmm5srqe0jji8va549bbd9.apps.googleusercontent.com',
+        'requestvisibleactions': 'http://schemas.google.com/AddActivity',
+        'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email',
+        'cookiepolicy': 'single_host_origin'
+      });
     };
 
     $scope.processUserInfo = function(userInfo) {
-      userParams = {
-        email: userInfo.emails[0].value,
-        password: userInfo.id
-      }
-
-      user = new AuthenticateUser({
-        spree_user: userParams
-      });
-
-      user.$save(function(response) {
-        if(response.error) {
-          $scope.registerUser(userParams);
-        } else {
-          $window.sessionStorage.setItem('user', JSON.stringify(response.user));
-          $location.path('/home');
+      $scope.$apply(function() {
+        userParams = {
+          email: userInfo.emails[0].value,
+          password: userInfo.id
         }
-      }, function(error) {
-        $scope.registerUser(userParams);
+
+        user = new AuthenticateUser({
+          spree_user: userParams
+        });
+
+        user.$save(function(response) {
+          if(response.error) {
+            $scope.registerUser(userParams);
+          } else {
+            $window.sessionStorage.setItem('user', JSON.stringify(response.user));
+            $location.path('/home');
+          }
+        }, function(error) {
+          $scope.registerUser(userParams);
+        });
       });
     };
 
@@ -88,18 +80,12 @@ App.controller('AuthenticationsCtrl', ['$scope', '$routeParams', '$location', '$
       });
     };
 
-    $scope.userInfoCallback = function(userInfo) {
-      $scope.$apply(function() {
-        $scope.processUserInfo(userInfo);
-      });
-    };
-
     $scope.getUserInfo = function() {
       gapi.client.request(
         {
           'path':'/plus/v1/people/me',
           'method':'GET',
-          'callback': $scope.userInfoCallback
+          'callback': $scope.processUserInfo
         }
       );
     };

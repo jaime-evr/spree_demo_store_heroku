f8bbd777866420bc57387736dd85c07d5a40cb8d
Jaime Victoria <jaime.victoria@crowdint.com>
2015-02-23 00:39:11 -0600

    cache products

--- app/assets/javascripts/angular_application/services/product.js
+++ app/assets/javascripts/angular_application/services/product.js
@@ -1,7 +1,7 @@
 App.factory('Product', ['$resource',
   function($resource) {
     return $resource('/api/products/:id', {}, {
-      query: { params: { isArray: true } }
+      query: { params: { isArray: true }, cache: true },
     });
   }
 ]);
